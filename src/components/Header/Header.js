1f91d9f9d930813b35c43b44a7fa0c03580bf61b
Sazzad25 <mdsazzadhosentamim95@gmail.com>
2022-09-29 21:54:34 +0600

    change title

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,15 +10,6 @@ const Header = () => {
             <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
             <h1>Dynamic Responsive Website</h1>
         </div>
-        // <nav className='header'>
-        //     <img src={logo} alt="" />
-        //     <div>
-        //         <a href="/shop">Shop</a>
-        //         <a href="/orders">Orders</a>
-        //         <a href="/inventory">Inventory</a>
-        //         <a href="/about">About</a>
-        //     </div>
-        // </nav>
     );
 };
 
